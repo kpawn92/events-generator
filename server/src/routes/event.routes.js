4553bf6826bcf49902b0764fa3236fbe842725ea
kpawn92 <hap921020@gmail.com>
2022-12-20 14:10:26 -0500

    Coment route event

--- server/src/routes/event.routes.js
+++ server/src/routes/event.routes.js
@@ -4,6 +4,14 @@ import { isModelator, verifyToken } from '../middlewares/authJwt.js'
 
 const router = Router();
 
+
+// TODO: Todos los usuarios obtienen los eventos y su descripcion a travez del ID del event
+/**
+ * @coment : Moderator crea el event (verificacion de token y rol)
+ * @coment : Moderator actualiza el event (verificacion de token y rol)
+ * @coment : Creating Subscribers sera free, a travez de una url sin headers and sin roles
+ */
+
 router.get('/', eventCtrl.getEvents)
 router.get('/:eventId', eventCtrl.getEventById)
 router.post('/', [verifyToken, isModelator], eventCtrl.createEvent)
