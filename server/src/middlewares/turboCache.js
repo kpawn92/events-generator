4047473adf5e7705503771f579ecf959523f2915
kpawn92 <hap921020@gmail.com>
2022-12-21 11:36:11 -0500

    refactor cache

--- server/src/middlewares/turboCache.js
+++ server/src/middlewares/turboCache.js
@@ -1,7 +1,9 @@
 import getExpeditiousCache from 'express-expeditious';
+import expeditiousMemoryEngine from 'expeditious-engine-memory';
 
 const defaultOptions = {
     namespace: 'expresscache',
+    engine: expeditiousMemoryEngine(),
     defaultTtl: '5 minute',
     statusCodeExpires: {
         404: '5 minutes',
