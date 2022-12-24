0cbde7312245c186f62df8f3853bc4d61c96f69b
kpawn92 <hap921020@gmail.com>
2022-12-24 12:15:27 -0500

    models and controllers for entity digest.instance

--- server/src/controllers/digestinstance.controller.js
+++ server/src/controllers/digestinstance.controller.js
@@ -1,2 +1,23 @@
-export const getDigestInsntances = async (req, res) => { };
-export const setDigestInsntance = async (req, res) => { };
+import { DigestInstance } from '../models/entity'
+
+export const getDigestInsntances = async (req, res) => {
+    const result = await DigestInstance.getInstances()
+    return res.status(200).json(result)
+};
+export const setDigestInsntance = async (req, res) => {
+    const result = await DigestInstance.create(req.body)
+    return res.status(200).json(result)
+};
+
+export const getStatusByIdSubs = async (req, res) => {
+    const { subsId } = req.params
+
+    const result = await DigestInstance.getStatusBySubscriber(subsId)
+    return res.status(200).json(result);
+};
+
+export const updateStateById = async (req, res) => {
+    const { instanceId } = req.params
+    const result = await DigestInstance.updateStatusById(instanceId, 1)
+    res.status(200).json(result)
+};
--- server/src/routes/digestinstance.routes.js
+++ server/src/routes/digestinstance.routes.js
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import { validateDigestInstanceDTO } from '../validators/digestinstance.validate.dto'
 import * as digestInstanceCtrl from '../controllers/digestinstance.controller'
 const router = Router();
 
 // TODO: Gestionar las instancias de los resumenes de trabajos mandados
 /**
- * @coment : Subscriber obtiene las instancias creadas para verificar su validez.
- * @coment : Subscriber crea las instancias.
+ * @coment : Subscriber crea las instancia.
+ * @coment : Subscriber obtiene el estado de las instancias creadas para verificar su validez.
+ * @coment : Moderator revisa los resumenes y edita el estado de las instancias para confirmar su validez.
+ * @coment : Las instancias solo seran almancenadas en la base de datos en un periodo vigente de 5 dias.
  */
 
+router.post('/', validateDigestInstanceDTO, digestInstanceCtrl.setDigestInsntance)
+router.get('/:subsId', digestInstanceCtrl.getStatusByIdSubs)
 router.get('/', digestInstanceCtrl.getDigestInsntances)
-router.post('/', digestInstanceCtrl.setDigestInsntance)
-
+router.put('/:instanceId', digestInstanceCtrl.updateStateById)
 export default router;
